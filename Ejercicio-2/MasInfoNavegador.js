d6a3b358a82f79836722fec721c79ed6dcee072d
uo277776 <90835490+uo277776@users.noreply.github.com>
2021-11-26 15:38:27 +0100

    Delete Ejercicio-2 directory

--- Ejercicio-2/MasInfoNavegador.js
+++
@@ -1,6 +0,0 @@
-document.write("MAS INFORMACION: ");
-document.write("Version: " + infoNavegador.version + ", ");
-document.write("Plataforma: " + infoNavegador.plataforma + ", ");
-document.write("Vendedor: " + infoNavegador.vendedor + ", ");
-document.write("Agente: " + infoNavegador.agente + ", ");
-document.write("Java Habilitado: " + infoNavegador.java + ", "); 
